60e034f0eb6d5bd9d1e525e957c40c3ed81f35aa
sami2928 <samiulhaq2928@hotmail.com>
2024-09-15 21:12:06 +0500

    updated whatsapp url

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,49 @@
-import { FaWhatsapp, FaLinkedin, FaTwitter, FaInstagram, FaFacebook } from 'react-icons/fa'
+import {
+  FaWhatsapp,
+  FaLinkedin,
+  FaTwitter,
+  FaInstagram,
+  FaFacebook,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className='max-w-[1300px] mx-auto flex justify-between p-6 md:p-20 text-sm md:text-lg mt-12'>
-        <div className='space-y-4'>
-            <h3 className='text-2xl text-gray-200 font-semibold'>Sami Ul Haq</h3>
-            <div className='flex flex-row gap-6 text-gray-400 text-4xl'>
-                <a href="https://web.whatsapp.com" target='_blank'><FaWhatsapp/></a>
-                <a href="https://www.linkedin.com/in/sami-ul-haq-a32741214/" target='_blank'><FaLinkedin/></a>
-                <a href="https://x.com/SamiUlH31944468?t=LvyvXK9MKcgVytSsqqUaLg&s=08" target='_blank'><FaTwitter/></a>
-                <a href="https://www.instagram.com/samiulhaq307?utm_source=qr&igsh=MTZkdW5ia2diZGpsYw==" target='_blank'><FaInstagram/></a>
-                <a href="https://www.facebook.com/profile.php?id=100017736079297" target='_blank'><FaFacebook/></a>
-            </div>
+    <div className="max-w-[1300px] mx-auto flex justify-between p-6 md:p-20 text-sm md:text-lg mt-12">
+      <div className="space-y-4">
+        <h3 className="text-2xl text-gray-200 font-semibold">Sami Ul Haq</h3>
+        <div className="flex flex-row gap-6 text-gray-400 text-4xl">
+          <a href="https://wa.me/923234200929" target="_blank">
+            <FaWhatsapp />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sami-ul-haq-a32741214/"
+            target="_blank"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://x.com/SamiUlH31944468?t=LvyvXK9MKcgVytSsqqUaLg&s=08"
+            target="_blank"
+          >
+            <FaTwitter />
+          </a>
+          <a
+            href="https://www.instagram.com/samiulhaq307?utm_source=qr&igsh=MTZkdW5ia2diZGpsYw=="
+            target="_blank"
+          >
+            <FaInstagram />
+          </a>
+          <a
+            href="https://www.facebook.com/profile.php?id=100017736079297"
+            target="_blank"
+          >
+            <FaFacebook />
+          </a>
         </div>
-        <p className='text-gray-400'>@2024 Sami Ul Haq</p>
+      </div>
+      <p className="text-gray-400">@2024 Sami Ul Haq</p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
